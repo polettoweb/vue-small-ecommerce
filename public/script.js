ddb13794d346484a7fcb5090f2ab32896751581c
Marco <marco@marcopoletto.co.uk>
2018-05-30 13:52:22 +0100

    Final touches on seach and style

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 var PRICE = 9.99;
+var LOAD_NUM = 10;
 
 new Vue({
   el: "#app",
@@ -6,20 +7,41 @@ new Vue({
     total: 0,
     items: [],
     cart: [],
+    results: [],
     newSearch: "web development",
     lastSearch: "",
     loading: false,
     price: PRICE
   },
+  computed: {
+    noMoreItems: function() {
+      return (
+        this.results.length === this.items.length && this.results.length > 0
+      );
+    }
+  },
   methods: {
+    appendItems: function() {
+      if (this.items.length < this.results.length) {
+        var append = this.results.slice(
+          this.items.length,
+          this.items.length + LOAD_NUM
+        );
+        this.items = this.items.concat(append);
+      }
+    },
     onSubmit: function() {
-      this.items = [];
-      this.loading = true;
-      this.$http.get("/search/".concat(this.newSearch)).then(function(res) {
-        this.lastSearch = this.newSearch;
-        this.items = res.data;
-        this.loading = false;
-      });
+      if (this.newSearch.length) {
+        this.items = [];
+        this.loading = true;
+        this.$http.get("/search/".concat(this.newSearch)).then(function(res) {
+          this.lastSearch = this.newSearch;
+          this.results = res.data;
+          this.appendItems();
+          this.items = res.data.slice(0, LOAD_NUM);
+          this.loading = false;
+        });
+      }
     },
     addItem: function(index) {
       this.total += PRICE;
@@ -63,5 +85,11 @@ new Vue({
   },
   mounted: function() {
     this.onSubmit();
+    var vueInstance = this;
+    var elem = document.getElementById("product-list-bottom");
+    var watcher = scrollMonitor.create(elem);
+    watcher.enterViewport(function() {
+      vueInstance.appendItems();
+    });
   }
 });
